50d9b792880b7bfc5b8f3194d8eb096485376fdb
luberf <luberf@live.com>
2019-11-28 09:29:13 -0500

    Proyecto terminado falta mejorar la interfaz

---
+++ app/src/main/java/com/example/yisela/mibusnavidrawer/ListaCarneActivity.java
@@ -0,0 +1,14 @@
+package com.example.yisela.mibusnavidrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaCarneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_carne);
+    }
+}
